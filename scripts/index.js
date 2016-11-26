7eb3d548173d7f482f0333cb00fbf7bcff5e19be
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2016-11-27 12:43:55 +1300

    add angular

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	
 	var image_path = '../imgs/'
-	var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg']
+	var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg']
 	var index = 0
 
 	displaySection()
