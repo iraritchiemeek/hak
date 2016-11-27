af52d59e981d211a9d6bc40b1679e04c51f6266b
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2016-11-27 18:54:40 +1300

    add about and top left title

--- scripts/index.js
+++ scripts/index.js
@@ -4,32 +4,8 @@ $(document).ready(function () {
 	var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg']
 	var index = 0
 
-	changeImageCount(index, images.length)
-
-	$('.gallery__arrow').click(function () {
-		if ($(this)[0].classList[1] ==  'gallery__arrow--back') {
-			if (index <= 0) {
-				index = images.length -1
-			} else {
-				index -= 1
-			}
-		} else {
-			if (index >= images.length -1) {
-				index = 0
-			} else {
-				index += 1
-			}
-		}
-		$('.gallery__img').attr("src", image_path + images[index])
-		changeImageCount(index, images.length)
-	})
-
 	$('.nav-bar__item').click(function(e) {
 		$('.nav-bar__item').removeClass('nav-bar__item--active')
 		$(this).addClass($(this)[0].classList[0] + '--active')
 	})
-
-	function changeImageCount(index, image_count) {
-		$('.gallery__img-count > span').text(index + 1 + '/' + image_count)
-	}
 })
