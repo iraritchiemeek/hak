aa5a986e3631754c578a649f4fa8c4e7d766d405
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2016-11-27 13:21:36 +1300

    add incement/deceremtn index angular funcions

--- scripts/angular.js
+++ scripts/angular.js
@@ -23,7 +23,29 @@ hakApp.config(function($routeProvider) {
 });
 
 hakApp.controller('mainController', function($scope, $location) {
+    $scope.gallery = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg']
+    $scope.index = 0
+
     $scope.go = function (path) {
       $location.path(path);
     };
+
+    $scope.decrementIndex = function(array) {
+        if ($scope.index <= 0) {
+            $scope.index = array.length -1
+        } else {
+            $scope.index -= 1
+        }
+    }
+
+    $scope.incrementIndex = function(array) {
+        if ($scope.index >= array.length -1) {
+            $scope.index = 0
+        } else {
+            $scope.index += 1
+        }
+    }
+
+
+
 });
