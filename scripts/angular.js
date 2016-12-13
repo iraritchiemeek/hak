215c66f7339d1b2b2322bb9deda22dd4d334ee2b
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2016-12-14 09:01:55 +1300

    add new images and optimise all images

--- scripts/angular.js
+++ scripts/angular.js
@@ -24,7 +24,7 @@ hakApp.config(function($routeProvider) {
 
 hakApp.controller('mainController', function($scope, $location) {
     $scope.video_ids = ['VN4oEB-qtrM', 'b_jx0XvpTVo']
-    $scope.gallery = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg']
+    $scope.gallery = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg', '28.jpg', ]
     $scope.index = 0
 
     $scope.go = function (path) {
