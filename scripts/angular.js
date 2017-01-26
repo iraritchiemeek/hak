e534720efc0dcd54c57440980c74d244bc6a9d37
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-01-26 21:20:35 +1300

    redesing

--- scripts/angular.js
+++ scripts/angular.js
@@ -24,8 +24,10 @@ hakApp.config(function($routeProvider) {
 
 hakApp.controller('mainController', function($scope, $location) {
     $scope.video_ids = ['VN4oEB-qtrM', 'b_jx0XvpTVo']
-    $scope.gallery = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg', '28.jpg', ]
+    $scope.gallery = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg', '28.jpg', ]  
     $scope.index = 0
+    $scope.playing_track = true
+    $scope.current_track
 
     $scope.go = function (path) {
       $location.path(path);
@@ -55,14 +57,24 @@ hakApp.controller('mainController', function($scope, $location) {
         }
     }
 
-    $scope.playTrack = function($event, bool) {
-        if (bool === true) {
-            var audio = new Audio('../public/album/' + $event.target.parentElement.lastElementChild.innerText + '.mp3')
-            console.log(audio)
-            audio.play()
-            // console.log($event.target.parentElement.lastElementChild.innerText)
+    $scope.setTrack = function ($event) {
+        // get track number
+        // use array of track names to set audio object
+        // check if track is already loaded
+    }
+
+    $scope.playTrack = function($event) {
+        $scope.playing_track = !$scope.playing_track
+        if (!$scope.playing_track) {
+            $($event.target).addClass('music__track-number--pause')
+            $scope.current_track = new Audio('../public/album/' + $event.target.parentElement.lastElementChild.innerText + '.mp3')     
+            $scope.current_track.play()
+            return
         } else {
-            // pause track
+            $scope.current_track.pause()
+            $($event.target).removeClass('music__track-number--pause')
+            console.log($scope.playing_track)
+            return
         }
     }
 
