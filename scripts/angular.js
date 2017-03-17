175749ecb51175d3c5fe871f06a972bb3e6e11f1
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-03-17 15:21:03 +1300

    Trying to fix git bug

--- scripts/angular.js
+++ scripts/angular.js
@@ -22,7 +22,7 @@ hakApp.config(function($routeProvider) {
     });
 });
 
-hakApp.controller('mainController', function($scope, $location) {
+hakApp.controller('mainController', function($scope, $location, $sce) {
     $scope.video_ids = ['VN4oEB-qtrM', 'b_jx0XvpTVo']
     $scope.gallery = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg', '28.jpg', ]  
     $scope.tracks = ['Aura', 'Look Forward To', 'She Say', 'Hues', 'No Days Off', 'Ambrosia', 'Solitude', 'Coral', 'Pollen', 'Lenox', 'Concrete Waves', 'Bedroom Record', '432 Hz', 'Order In Nature', 'June']
@@ -50,6 +50,18 @@ hakApp.controller('mainController', function($scope, $location) {
         }
     }
 
+    $scope.selectVideoButton = function (video) {
+        if (video == 'aura') {
+            $($('.videos__button')[1]).css({opacity: 0.6})
+            $($('.videos__button')[0]).css({opacity: 1})
+            $scope.current_video_url = $sce.trustAsResourceUrl('https://www.youtube.com/embed/VN4oEB-qtrM')
+        } else if (video == 'solitude') {     
+            $($('.videos__button')[0]).css({opacity: 0.6})
+            $($('.videos__button')[1]).css({opacity: 1})
+            $scope.current_video_url = $sce.trustAsResourceUrl('https://www.youtube.com/embed/b_jx0XvpTVo')
+        }
+    }
+
     $scope.displayPlayIcon = function (index) {
         $($('.music__track-number')[index]).addClass('music__track-number--play')
     }
@@ -59,8 +71,12 @@ hakApp.controller('mainController', function($scope, $location) {
     }
 
     $scope.clearIcons = function() {
-        $('.music__track-number').removeClass('music__track-number--pause')
-        $('.music__track-number').removeClass('music__track-number--play')
+        for (var i = $('.music__track-number').length - 1; i >= 0; i--) {
+            if ($($('.music__track-number')[i]).attr('id') !== $scope.current_track_index) {                
+                $('.music__track-number').removeClass('music__track-number--pause')
+                $('.music__track-number').removeClass('music__track-number--play')
+            }
+        }
     }
 
     // $scope.setTrack = function ($event) {
@@ -74,13 +90,12 @@ hakApp.controller('mainController', function($scope, $location) {
             $scope.setTrack(index)
             $scope.togglePlaying(index)
             $scope.displayPauseIcon(index)
-        } else if ($($('.music__track-number')[index]).attr('id') == $scope.currentTrackIndex) {
-            console.log(index)
-            console.log($($('.music__track-number')[1]).attr('id'))
+        } else if ($($('.music__track-number')[index]).attr('id') == $scope.current_track_index) {
             $scope.displayPauseIcon(index)
             $scope.togglePlaying(index)
         } else {
-            $scope.currentTrackIndex = index
+            $scope.clearIcons()
+            $scope.current_track_index == index
             $($scope.current_track).attr('src', '../public/album/' + $scope.tracks[index] + '.mp3')
             $($scope.current_track)[0].load()
             $($scope.current_track)[0].pause()
@@ -98,9 +113,11 @@ hakApp.controller('mainController', function($scope, $location) {
         if ($scope.playing_track) {
             $scope.playing_track = false
             $scope.current_track.pause();
+            $scope.displayPlayIcon(index)
         } else {
             $scope.playing_track = true
             $scope.current_track.play();
+            $scope.displayPauseIcon(index)
         }
     }
 
